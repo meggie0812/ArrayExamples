7bab8d15e03a9129cd713bd472a0dc557d8f66d9
meggie0812 <31289949+meggie0812@users.noreply.github.com>
2018-11-15 11:25:24 -0800

    frq

---
+++ DigitDriver.java
@@ -0,0 +1,11 @@
+public class DigitDriver
+{
+    
+    public static void main(String[] args)
+    {
+        Digits digits = new Digits(13456);
+        digits.isStrictlyIncreasing();
+        
+    }
+}
+
---
+++ Digits.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+public class Digits
+{
+    private ArrayList<Integer> digitList;
+    
+    public Digits()
+     {
+         
+     }
+    
+    public Digits(int num)
+     {
+         digitList = new ArrayList<Integer>();
+         
+         int x=num; //not touching num
+         
+         if(num==0)
+           digitList.add( new Integer(0) );
+         
+         
+         while(x>0)
+         {
+             int n=(x%10);
+             digitList.add(0,new Integer(n));
+             x=x/10;
+                 
+         }
+           System.out.println(digitList);
+     }
+     
+     public boolean isStrictlyIncreasing()
+    {
+        for(int i=0; i<digitList.size()-1; i++)
+        {
+            if(digitList.get(i+1)<=digitList.get(i))
+            {
+                return false;
+            }
+            
+        }
+        return true;
+    } 
+}
